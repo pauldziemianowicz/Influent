782ced340e98c0b3c50328c6fc7c55d7da116533
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-14 16:28:30 -0600

    influencer/assessment routes

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,53 @@
-var app = angular.module('Influent',[])
+var app = angular.module('Influent',['ui.router'])
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+    .state('defaultHome', {
+      url: '/',
+      templateUrl: "templates/default_home.html"
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: "templates/about.html"
+    })
+    .state('directoryMain', {
+      url: '/directory',
+      templateUrl: "templates/directory_main.html"
+    })
+    .state('directoryInfluencers', {
+      url: '/directory/influencers',
+      templateUrl: "templates/directory_influencers.html"
+    })
+    .state('directoryBrands', {
+      url: '/directory/brands',
+      templateUrl: "templates/directory_brands.html"
+    })
+    .state('influencerOverview', {
+      url: '/dashboard/overview',
+      templateUrl: "templates/influencer_dashboard_overview.html"
+    })
+    .state('influencerProfile', {
+      url: '/dashboard/profile',
+      templateUrl: "templates/influencer_dashboard_profile.html"
+    })
+    .state('influencerCommunication', {
+      url: '/dashboard/communication',
+      templateUrl: "templates/influencer_dashboard_communications.html"
+    })
+    .state('influencerMessages', {
+      url: '/dashboard/communication/messages',
+      templateUrl: "templates/influencer_dashboard_messages.html"
+    })
+    .state('influencerOffers', {
+      url: '/dashboard/communication/offers',
+      templateUrl: "templates/influencer_dashboard_offers.html"
+    })
+    .state('influencerJobs', {
+      url: '/dashboard/jobs',
+      templateUrl: "templates/influencer_dashboard_jobs.html"
+    })
+
+    // $locationProvider.html5Mode(true);
+})
---
+++ public/app/directives/directives.js
@@ -0,0 +1,83 @@
+app.directive('mastheadNav', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/masthead_nav.html'
+  }
+})
+
+app.directive('aboutPage', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/about.html'
+  }
+})
+
+app.directive('mainDirectory', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/main_directory.html'
+  }
+})
+
+app.directive('influencerDirectory', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/influencer_directory.html'
+  }
+})
+
+app.directive('brandDirectory', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/brand_directory.html'
+  }
+})
+
+app.directive('influencerDashboard', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/influencer_dashboard.html'
+  }
+})
+
+app.directive('influencerOverview', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/influencer_overview.html'
+  }
+})
+
+app.directive('influencerProfile', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/influencer_profile.html'
+  }
+})
+
+app.directive('influencerJobs', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/influencer_jobs.html'
+  }
+})
+
+app.directive('influencerCommunications', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/influencer_communications.html'
+  }
+})
+
+app.directive('influencerMessages', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/influencer_messages.html'
+  }
+})
+
+app.directive('influencerOffers', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/partials/influencer_offers.html'
+  }
+})
