b3716ea9c6f432b05031986b6442305cbbbb15db
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-15 15:09:07 -0600

    changed functionality of instagram login

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,13 +1,12 @@
 app.factory('instagramAPI', ['$http', function($http) {
   var service = {};
   service.authenticateAccount = function() {
-    return $http.get("https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token")
+    $window.location.href="https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token";
   }
   service.returnInstagramClientId = function() {
     $http({
       method: "GET",
       url: "https://influent.herokuapp.com/api",
-      headers: {"Access-Control-Allow-Origin": "https://influent.herokuapp.com"}
     }).then(function(data){
       console.log(data);
       service.instagramClientId = data.data;
