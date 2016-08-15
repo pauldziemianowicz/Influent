cafb8d85951f05fe2e75b8086eb97b4e8785830e
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-15 14:45:58 -0600

    added cors header to request service

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -4,7 +4,11 @@ app.factory('instagramAPI', ['$http', function($http) {
     return $http.get("https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token")
   }
   service.returnInstagramClientId = function() {
-    $http.get("https://influent.herokuapp.com/api").then(function(data){
+    $http({
+      method: "GET",
+      url: "https://influent.herokuapp.com/api",
+      headers: {"Access-Control-Allow-Origin": "https://influent.herokuapp.com/"}
+    }).then(function(data){
       console.log(data);
       service.instagramClientId = data.data;
       console.log(service.instagramClientId);
