ff85159271e17f782fdf81dff2eeabcf9fc04b3d
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-15 14:28:41 -0600

    changed http request to https

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -4,7 +4,7 @@ app.factory('instagramAPI', ['$http', function($http) {
     return $http.get("https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token")
   }
   service.returnInstagramClientId = function() {
-    $http.get("http://127.0.0.1:3000/api").then(function(data){
+    $http.get("https://127.0.0.1:3000/api").then(function(data){
       service.instagramClientId = data.data;
     })
   }
