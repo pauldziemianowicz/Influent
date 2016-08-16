fa572a89162ada30ae31736a27dbf758fed5dade
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-16 16:07:42 -0600

    slight changes to access token logic

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -13,12 +13,7 @@ app.factory('instagramAPI', ['$q', '$window', '$http', '$timeout', function($q, 
       }
     }
 
-    return $q(function(resolve, reject){
-      $timeout(getAccessToken(){
-        // $window.location.href="https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token";
-        resolve('success');
-      }, 0)
-    });
+    $timeout(getAccessToken(), 0);
 
     // $window.location.href="https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token";
     // service.accessToken = $window.location.hash.split('').splice(15, $window.location.hash.length).join('');
