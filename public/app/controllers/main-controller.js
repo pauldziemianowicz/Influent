5526f29e23259c2bb341bb52111881c8c6994e47
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-16 16:44:41 -0600

    slight changes to access token logic

--- public/app/controllers/main-controller.js
+++ public/app/controllers/main-controller.js
@@ -7,16 +7,16 @@ app.controller('MainController', ['$scope', '$window', 'instagramAPI', '$timeout
   $scope.view.authenticateInstagram = function() {
     instagramAPI.authenticateAccount().then(function(data) {
       console.log(data);
+      $scope.data.accessToken = data
     })
     // console.log($window.location.hash.split('').splice(15, $window.location.hash.length).join(''));
     // $scope.data.accessToken = $window.location.hash.split('').splice(15, $window.location.hash.length).join('');
   };
 
   $scope.view.printAccessToken = function() {
-    console.log(instagramAPI.accessToken);
     // console.log($window.location.hash.split('').splice(15, $window.location.hash.length).join(''));
     // $scope.data.accessToken = $window.location.hash.split('').splice(15, $window.location.hash.length).join('');
-    // console.log($scope.data.accessToken);
+    console.log($scope.data.accessToken);
   };
 
   // $scope.view.apiAccessTokenTest = function() {
