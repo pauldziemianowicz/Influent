637367f17e5f65dd3ac477687ca62acdfae29a79
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-16 09:57:58 -0600

    updated access token logic to test

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,8 +1,9 @@
 app.factory('instagramAPI', ['$q', '$window', '$http', function($q, $window, $http) {
   var service = {};
-  service.accessToken = $window.location.hash.split('').splice(15, $window.location.hash.length).join('');
+  service.accessToken = "";
   service.authenticateAccount = function() {
     $window.location.href="https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token";
+    service.accessToken = $window.location.hash.split('').splice(15, $window.location.hash.length).join('');
   }
   service.printLocation = function() {
     console.log(service.accessToken);
