b31bb113e122c58f376b02b244676b93b1e288e6
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-16 09:45:22 -0600

    reorganized print access token logic

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,8 +1,8 @@
 app.factory('instagramAPI', ['$q', '$window', '$http', function($q, $window, $http) {
   var service = {};
+  service.accessToken = $window.location;
   service.authenticateAccount = function() {
     $window.location.href="https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token";
-    service.accessToken = $window.location;
   }
   service.printLocation = function() {
     console.log(service.accessToken);
