5c46c2f4907be5a132134a7e52430a1b490eafe5
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-16 10:03:30 -0600

    updated access token logic to test

--- public/app/controllers/main-controller.js
+++ public/app/controllers/main-controller.js
@@ -2,11 +2,17 @@ app.controller('MainController', ['$scope', 'instagramAPI', function($scope, ins
   instagramAPI.returnInstagramClientId();
 
   $scope.view = {};
+  $scope.data = {}
+
   $scope.view.authenticateInstagram = function() {
     instagramAPI.authenticateAccount();
+    $scope.data.accessToken = $window.location.hash.split('').splice(15, $window.location.hash.length).join('');
   }
   $scope.view.printAccessToken = function() {
-    instagramAPI.printLocation();
+    console.log($scope.data.accessToken);
+  }
+  $scope.view.apiAccessTokenTest = function() {
+    console.log(instagramAPI.accessToken);
   }
   $scope.view.returnInstagramClientId = function() {
     console.log(instagramAPI.instagramClientId);
