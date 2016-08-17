bde557706b23f0ab4c56a4194e98a60621b864bd
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-16 18:53:34 -0600

    slight changes to access token logic

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,18 +1,18 @@
 app.factory('instagramAPI', ['$q', '$window', '$http', '$timeout', '$interval', function($q, $window, $http, $timeout, $interval) {
   var service = {};
 
-  service.authenticateAccount = function() {
+  var validateAccessCodeReturn = $interval(function() {
+    console.log($window.location.hash.split('').splice(0, 14).join('') === "#access_token=");
+    if ($window.location.hash.split('').splice(0, 14).join('') === "#access_token=") {
+      console.log("it's a match!");
+      $interval.cancel(validateAccessCodeReturn)
+      return $window.location.hash.split('').splice(14, $window.location.hash.length).join('');
+      } else {
+        console.log("not yet!");
+      }
+    }, 100)
 
-    var validateAccessCodeReturn = $interval(function() {
-      console.log($window.location.hash.split('').splice(0, 14).join('') === "#access_token=");
-      if ($window.location.hash.split('').splice(0, 14).join('') === "#access_token=") {
-        console.log("it's a match!");
-        $interval.cancel(validateAccessCodeReturn)
-        return $window.location.hash.split('').splice(14, $window.location.hash.length).join('');
-        } else {
-          console.log("not yet!");
-        }
-      }, 100)
+  service.authenticateAccount = function() {
 
     function getAccessToken() {
       $window.location.href = "https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token";
