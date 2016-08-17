697602fc2bc87d8f57dfd505e2515e870f54699c
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-16 18:59:43 -0600

    slight changes to access token logic

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -3,23 +3,23 @@ app.factory('instagramAPI', ['$q', '$window', '$http', '$timeout', '$interval', 
 
   service.authenticateAccount = function() {
 
-    var validateAccessCodeReturn = $interval(function() {
-      console.log($window.location.hash.split('').splice(0, 14).join(''));
-      if ($window.location.hash.split('').splice(0, 14).join('') === "#access_token=") {
-        console.log("it's a match!");
-        $interval.cancel(validateAccessCodeReturn)
-        return $window.location.hash.split('').splice(14, $window.location.hash.length).join('');
-        } else {
-          console.log("not yet!");
-        }
-      }, 100)
-
     function getAccessToken() {
       $window.location.href = "https://api.instagram.com/oauth/authorize/?client_id=" + service.instagramClientId + "&redirect_uri=https://influent.herokuapp.com&response_type=token";
       console.log($window.location.href);
     }
 
     return $timeout(getAccessToken(), 0).then(function() {
+      
+      var validateAccessCodeReturn = $interval(function() {
+        console.log($window.location.hash.split('').splice(0, 14).join(''));
+        if ($window.location.hash.split('').splice(0, 14).join('') === "#access_token=") {
+          console.log("it's a match!");
+          $interval.cancel()
+          return $window.location.hash.split('').splice(14, $window.location.hash.length).join('');
+          } else {
+            console.log("not yet!");
+          }
+        }, 100)
       // return $window.location.hash.split('').splice(14, $window.location.hash.length).join('');
       validateAccessCodeReturn();
     })
