943f70fe478a7ab133db7dbd08e391e56d459323
Paul Dziemianowicz <pauldziemianowicz@gmail.com>
2016-08-17 12:14:34 -0600

    slight changes to access token logic

--- public/app/controllers/main-controller.js
+++ public/app/controllers/main-controller.js
@@ -5,6 +5,7 @@ app.controller('MainController', ['$scope', '$window', 'instagramAPI', '$timeout
   $scope.data = {}
   $scope.data.token = localStorage.influentInstagramToken;
   $scope.data.tokenInUrl = ($window.location.hash.split('').splice(0, 15).join(''))
+  $scope.data.tokenInUrlBool = ($window.location.hash.split('').splice(0, 15).join('') === "#/access_token=")
 
   $scope.view.authenticateInstagram = function() {
     instagramAPI.authenticateAccount().then(function(data) {
